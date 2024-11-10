3eddfe5273edd0da45024061d245d58957f2325f
Alessandro Thea <alessandro.thea@cern.ch>
2024-11-10 15:46:37 +0100

    Working on Kernel python bindings

--- pybindsrc/module.cpp
+++ pybindsrc/module.cpp
@@ -24,6 +24,7 @@ PYBIND11_MODULE(_daq_oks_py, m)
   m.doc() = "C++ implementation of the application dal modules";
 
   py::class_<OksFile>(m, "OksFile");
+  // py::class_<OksFile::Map>(m, "OksFileMap");
   py::class_<OksClass, std::unique_ptr<OksClass, py::nodelete>>(m, "OksClass")
       .def("get_name",&OksClass::get_name, py::return_value_policy::reference)
       .def("get_description",&OksClass::get_description, py::return_value_policy::reference)
@@ -42,29 +43,191 @@ PYBIND11_MODULE(_daq_oks_py, m)
       .def("get_domain_name",&OksKernel::get_domain_name)
       .def("get_user_name",&OksKernel::get_user_name)
       // ---
-      .def("load_file",&OksKernel::load_file, "name"_a, "bind"_a = true)
-      .def("load_schema",&OksKernel::load_schema, "name"_a, "parent"_a = nullptr)
-      .def("close_all_schema",&OksKernel::close_all_schema)
+      // .def("load_file",&OksKernel::load_file, "name"_a, "bind"_a = true)
+      // .def("load_schema",&OksKernel::load_schema, "name"_a, "parent"_a = nullptr)
+      // .def("close_all_schema",&OksKernel::close_all_schema)
 
       // ---
-      .def("load_data",&OksKernel::load_file, "name"_a, "bind"_a = true)
-      .def("close_all_data",&OksKernel::close_all_data)
+      // .def("load_data",&OksKernel::load_file, "name"_a, "bind"_a = true)
+      // .def("close_all_data",&OksKernel::close_all_data)
       // -- 
-      .def("classes",&OksKernel::classes, py::return_value_policy::reference)
-      .def("number_of_classes",&OksKernel::number_of_classes)
-      .def("objects",&OksKernel::objects, py::return_value_policy::reference)
-      .def("number_of_objects",&OksKernel::number_of_objects)
+      // .def("classes",&OksKernel::classes, py::return_value_policy::reference)
+      // .def("number_of_classes",&OksKernel::number_of_classes)
+      // .def("objects",&OksKernel::objects, py::return_value_policy::reference)
+      // .def("number_of_objects",&OksKernel::number_of_objects)
       //
       // .def("find_class",&OksKernel::find_class)
       // .def("find_object",&OksKernel::find_object, py::return_value_policy::reference)
 
       // 
-      .def("get_test_duplicated_objects_via_inheritance_mode", &OksKernel::get_test_duplicated_objects_via_inheritance_mode)
-      .def("set_test_duplicated_objects_via_inheritance_mode", &OksKernel::set_test_duplicated_objects_via_inheritance_mode)
-      .def("get_allow_duplicated_objects_mode", &OksKernel::get_allow_duplicated_objects_mode)
-      .def("set_allow_duplicated_objects_mode", &OksKernel::set_allow_duplicated_objects_mode)
+      // .def("get_test_duplicated_objects_via_inheritance_mode", &OksKernel::get_test_duplicated_objects_via_inheritance_mode)
+      // .def("set_test_duplicated_objects_via_inheritance_mode", &OksKernel::set_test_duplicated_objects_via_inheritance_mode)
+      // .def("get_allow_duplicated_objects_mode", &OksKernel::get_allow_duplicated_objects_mode)
+      // .def("set_allow_duplicated_objects_mode", &OksKernel::set_allow_duplicated_objects_mode)
 
+
+
+
+    // 	get_verbose_mode
+    // 	set_verbose_mode 
+    .def("get_verbose_mode",&OksKernel::get_verbose_mode)
+    .def("set_verbose_mode",&OksKernel::set_verbose_mode)
+
+    //  get_silence_mode
+    // 	set_silence_mode
+    .def("get_silence_mode",&OksKernel::get_silence_mode)
+    .def("set_silence_mode",&OksKernel::set_silence_mode)
+
+    // 	set_profiling_mode
+    .def("set_profiling_mode",&OksKernel::set_profiling_mode)
+
+    // 	get_allow_duplicated_classes_mode
+    // 	set_allow_duplicated_classes_mode
+    .def("get_allow_duplicated_classes_mode",&OksKernel::get_allow_duplicated_classes_mode)
+    .def("set_allow_duplicated_classes_mode",&OksKernel::set_allow_duplicated_classes_mode)
+
+    //  get_allow_duplicated_objects_mode 
+    //  set_allow_duplicated_objects_mode 
+    .def("get_allow_duplicated_objects_mode",&OksKernel::get_allow_duplicated_objects_mode)
+    .def("set_allow_duplicated_objects_mode",&OksKernel::set_allow_duplicated_objects_mode)
+
+    // 	get_test_duplicated_objects_via_inheritance_mode
+    // 	set_test_duplicated_objects_via_inheritance_mode
+    .def("get_test_duplicated_objects_via_inheritance_mode",&OksKernel::get_test_duplicated_objects_via_inheritance_mode)
+    .def("set_test_duplicated_objects_via_inheritance_mode",&OksKernel::set_test_duplicated_objects_via_inheritance_mode)
+
+    //! get_mutex 
+    // find_schema_file
+    // find_data_file
+    .def("find_schema_file",&OksKernel::find_schema_file)
+    .def("find_data_file",&OksKernel::find_data_file)
+
+    // create_list_of_schema_classes
+    // create_list_of_data_objects
+    .def("create_list_of_schema_classes",&OksKernel::create_list_of_schema_classes, py::return_value_policy::reference_internal)
+    .def("create_list_of_data_objects",&OksKernel::create_list_of_data_objects, py::return_value_policy::reference_internal)
+
+    // 	create_file_info
+    .def("create_file_info",&OksKernel::create_file_info, py::return_value_policy::reference_internal)
+
+    // 	get_file_path
+    .def("get_file_path",&OksKernel::get_file_path, "path"_a, "parent_file"_a = nullptr, "strict_path"_a=true)
+
+    // get_repository_version
+    .def("get_repository_version",&OksKernel::get_repository_version)
+
+    // is_user_repository_created()
+    .def("is_user_repository_created",&OksKernel::is_user_repository_created)
+
+    // get_user_repository_root
+    // set_user_repository_root
+    .def("get_user_repository_root",&OksKernel::get_user_repository_root)
+    .def("set_user_repository_root",&OksKernel::set_user_repository_root, "path"_a, "version"_a = true)
+
+    // get_includes
+    .def("get_includes",&OksKernel::get_includes)
+    // k_close_dangling_includes
+    
+    // load_file
+    .def("load_file",&OksKernel::load_file, "name"_a, "bind"_a = true, py::return_value_policy::reference_internal)
+    // load_schema
+    .def("load_schema",&OksKernel::load_schema, "name"_a, "parent"_a = nullptr, py::return_value_policy::reference_internal)
+    // new_schema
+    .def("new_schema",&OksKernel::new_schema, "name"_a, py::return_value_policy::reference_internal)
+    // save_schema
+    // .def("save_schema",py::overload_cast<OksFile*, bool, OksFile*>(&OksKernel::save_schema), "file_h"_a, "force"_a = false, "true_file_h"_a = nullptr)
+    // .def("save_schema",py::overload_cast<OksFile*, bool, const OksClass::Map&>(&OksKernel::save_schema), "file_h"_a, "force"_a, "classes")
+    // backup_schema
+    .def("backup_schema",&OksKernel::backup_schema, "pf"_a, "suffix"_a = ".bak")
+    // save_as_schema
+    .def("save_as_schema",&OksKernel::save_as_schema, "name"_a, "file_h"_a)
+    // save_all_schema
+    .def("save_all_schema",&OksKernel::save_all_schema)
+    // close_schema
+    .def("close_schema",&OksKernel::close_schema,  "file_h"_a)
+    // close_all_schema
+    .def("close_all_schema",&OksKernel::close_all_schema)
+    // set_active_schema
+    .def("set_active_schema",&OksKernel::set_active_schema, "file_h"_a)
+    // get_active_schema
+    .def("get_active_schema",&OksKernel::get_active_schema)
+    // schema_files
+    .def("schema_files",&OksKernel::schema_files)
+    
+    //! create_list_of_upsated_schema_file
+    //! get_updated_repository_files
+
+    // load_data
+    .def("load_data",&OksKernel::load_data, "name"_a, "bind"_a = true)
+    // reload_data
+    .def("reload_data",&OksKernel::reload_data, "files"_a, "allow_schema_extension"_a = true)
+    
+    // new_data
+    .def("new_data",&OksKernel::new_data, "name"_a, "logical_name"_a = "", "type"_a = "", py::return_value_policy::reference_internal)
+
+    // // save_data
+    // // backup_data
+    // // save_as_data
+    // // save_all_sata
+    // // close_data
+    // // close_all_data
+    // .def("close_all_data",&OksKernel::close_all_data)
+
+    // // set_active_data
+    // // get_active_daat
+    // // data_files
+    // // create_list_of_updated_data_files
+    // // get_modified_files
+    // // get_repository_dirs
+    // // commit_repository
+    // // tag_repository
+    // // get_repository_checkout_ts
+    // // update_repository
+    // // get_repository_versions_diff
+    // // get_repository_unmerged_files
+    // // get_repository_versions
+    // // get_repository_versions_by_hash
+    // // get_repository_versions_by_date
+    // // read_repository_version
+    // // insert_repository_dir
+    // // remove_repository_dir 
+    // // classes
+    .def("classes",&OksKernel::classes, py::return_value_policy::reference)
+    // number_of_classes
+    .def("number_of_classes",&OksKernel::number_of_classes)
+    // objects
+    .def("objects",&OksKernel::objects, py::return_value_policy::reference)
+    // number_of_objects
+    .def("number_of_objects",&OksKernel::number_of_objects) 
+    // // find_class
+    // // get_all_classes
+    // // registrate_all_classes
+    // // is_dangling
+    // // subscribe_create_class
+    // // subscribe_change_class
+    // // subscribe_create_object
+    // // subscribe_delete_object
+    // // bind_objects
+    // // get_bind_object_status
+    // // get_bind_classes_status
+    // // unset_repository_created
+
+
+    // // GetVersion
+    // // get_host_name
+    // // get_domain_name
+    // // get_user_name
+    // // get_skip_string_range
+    // // set_skip_string_range
+    // // check_read_only
+    // // get_repository_root
+    // // get_repository_mapping_dir
+    // // get_tmp_file
+    // // get_cwd
+    // // reset_cwd
+    // // set_use_string_repository_path
     ;
+
 
 }
 
--- test/apps/oks_check_inheritance.cxx
+++ test/apps/oks_check_inheritance.cxx
@@ -4,6 +4,66 @@
 #include <fmt/core.h>
 #include <deque>
 
+namespace dunedaq {
+namespace oks {
+
+
+std::deque<std::set<OksClass*>> construct_class_domains(const OksKernel& k) {
+
+  std::deque<std::set<OksClass*>> domains;
+
+  // Find classes with no superclasses.
+  // They are domain (or inheritance cluster) seeds
+  std::deque<OksClass*> progenitors;
+  for ( auto const& [key, kl] : k.classes()) {
+      if (kl->direct_super_classes() == nullptr) {
+          progenitors.push_back(kl);
+      }
+  }
+
+
+  // Loop over seeds
+  for( auto kl : progenitors ) {
+    
+    // Make a candidate cluster based using the seed subclasses
+    std::set<OksClass*> klass_cluster;
+    klass_cluster.insert(kl);
+    klass_cluster.insert(kl->all_sub_classes()->begin(), kl->all_sub_classes()->end());
+    
+    // Look for overlaps with other domains
+    std::deque<std::set<OksClass*>> overlapping;
+    for( auto& dom : domains ) {
+      std::set<OksClass*> intersection;
+      std::set_intersection(dom.begin(), dom.end(), klass_cluster.begin(), klass_cluster.end(),
+        std::inserter(intersection, intersection.begin()));
+      // non-zero intersection, overlap found
+      if (intersection.size() > 0) {
+        overlapping.push_back(dom);
+      }
+    }
+
+    // If overlapping are found, merge all overlapping domains
+    if ( overlapping.size() > 0 ) {
+      for( auto& dom : overlapping ) {
+        // merge the existing cluster in klass_cluster
+        klass_cluster.insert(dom.begin(), dom.end());
+        // Remove the old cluster from the list
+        auto it = std::find(domains.begin(), domains.end(), dom);
+        if (it!= domains.end()) {
+            domains.erase(it);
+        }
+      }
+    }
+
+    domains.push_back(klass_cluster);
+
+  }
+  
+  return domains;
+}
+
+} // namespace oks
+} // namespace dunedaq
 
 int main(int argc, char const *argv[])
 {
@@ -28,73 +88,25 @@ int main(int argc, char const *argv[])
 
     k.load_schema(file);
 
-    // Find classes with no superclasses.
-    // They are domain (or inheritance cluster) seeds
-    std::deque<OksClass*> top_classes;
-    for ( auto const& [key, kl] : k.classes()) {
-        if (kl->direct_super_classes() == nullptr) {
-            top_classes.push_back(kl);
-        }
-    }
+    auto domains = construct_class_domains(k);
 
-    // TODO: remove 
-    fmt::print("Initial cluster seeds\n");
-    for( auto kl : top_classes ) {
-        fmt::print("   {}\n", kl->get_name());
-    }
-
-    // Build clusters
-    std::deque<std::set<OksClass*>> clusters;
-
-    // Loop over seeds
-    for( auto kl : top_classes ) {
-      
-      // Make a candidate cluster based using the seed subclasses
-      std::set<OksClass*> klass_cluster;
-      klass_cluster.insert(kl);
-      klass_cluster.insert(kl->all_sub_classes()->begin(), kl->all_sub_classes()->end());
-      
-      // Look for overlaps with other domains
-      std::deque<std::set<OksClass*>> overlaps;
-      for( auto& cl : clusters ) {
-        std::set<OksClass*> intersection;
-        std::set_intersection(cl.begin(), cl.end(), klass_cluster.begin(), klass_cluster.end(),
-          std::inserter(intersection, intersection.begin()));
-        // non-zero intersection, overlap found
-        if (intersection.size() > 0) {
-          overlaps.push_back(cl);
-        }
-      }
-
-      // If overlaps are found, merge all overlapping domains
-      if ( overlaps.size() > 0 ) {
-        for( auto& cl : overlaps ) {
-          // merge the existing cluster in klass_cluster
-          klass_cluster.insert(cl.begin(), cl.end());
-          // Remove the old domain from the list
-          auto it = std::find(clusters.begin(), clusters.end(), cl);
-          if (it!= clusters.end()) {
-              clusters.erase(it);
-          }
-        }
-      }
-
-      clusters.push_back(klass_cluster);
-
-    }
+    std::map<const OksClass*, uint> class_domain_map;
 
 
     // Print the clustered domains
-    fmt::print("Found {} inheritance clusters\n", clusters.size());
-    for( size_t i(0); i<clusters.size(); ++i ) {
-      auto& cl = clusters[i];
+    fmt::print("Found {} inheritance domains\n", domains.size());
+    for( size_t i(0); i<domains.size(); ++i ) {
+      auto& d = domains[i];
       fmt::print("   - {} : [", i);
-      for( const auto* kl : cl ) {
+      for( const auto* kl : d ) {
         fmt::print(" {},", kl->get_name());
+        class_domain_map[kl] = i;
       }
       fmt::print("]\n");
     }
 
+
+  
 
     /* code */
     return 0;
